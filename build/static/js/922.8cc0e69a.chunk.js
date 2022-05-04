d61f6a7d3ca9504c4c03417b1364708005864e22
Somnath Sarkar <sosarkar@amazon.com>
2022-05-04 12:54:14 -0400

    Upload build to Github

---
+++ build/static/js/922.8cc0e69a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpersonal_site=self.webpackChunkpersonal_site||[]).push([[922],{8922:function(e,n,s){s.r(n);s(2791);var t=s(1523),r=s(6907),i=s(184);n.default=function(){return(0,i.jsxs)("div",{className:"not-found",children:[(0,i.jsx)(r.ql,{title:"404"}),(0,i.jsx)("h1",{children:"Page Not Found."}),(0,i.jsxs)("p",{children:["Return to ",(0,i.jsx)(t.rU,{to:"/",children:"index"}),"."]})]})}}}]);
+//# sourceMappingURL=922.8cc0e69a.chunk.js.map
